b65a0c6b5691190101cbd24f05a28411c3b2a186
Angelo Neacsu <angelo.neacsu@gmail.com>
2017-12-10 00:32:17 +0200

    updated mysql/unit_test.sh

--- mysql/unit_test.sh
+++ mysql/unit_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mysqladmin -h docker.angeloneacsu.com -u root --password=pass -P 53306 status
+mysqladmin -h docker.angeloneacsu.com -u root --password=pass -P 53306 create databasename db1 \
+&& \
+mysql --host=docker.angeloneacsu.com -P 53306 -u root --password=pass db1  < mysql/unit_test.sql
 
-mysql --host=docker.angeloneacsu.com -P 53306 -u root --password=pass  < mysql/unit_test.sql
 
