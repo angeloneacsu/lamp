965b85192478323e5057c65a4a43b1747398debc
Angelo Neacsu <angelo.neacsu@gmail.com>
2017-12-09 20:16:50 +0200

    updated apache/unit_test.sh

--- apache/unit_test.sh
+++ apache/unit_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #docker run docker.angeloneacsu.com/kilabs/lamp-apache:latest echo "Unit test"
-docker run -d --name lamp-apache-unittest -p 4504:80 docker.angeloneacsu.com/kilabs/lamp-apache:latest 
+docker run -d --name lamp-apache-unittest -p 44606:80 docker.angeloneacsu.com/kilabs/lamp-apache:latest 
 
-apt-get update; apt-get install -y curl; 
-curl -Lv http://localhost:4504
+#apt-get update; apt-get install -y curl; 
+# because curl runs inside jenkins container, it needs to know the external IP address of the lamp-apache container (ex: 44606)
+curl -Lv http://localhost:44606
 
 docker rm -f lamp-apache-unittest
