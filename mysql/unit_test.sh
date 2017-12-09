267bf22bd4cb31c44b9e1fd3a6846240e6a28898
Angelo Neacsu <angelo.neacsu@gmail.com>
2017-12-09 22:54:08 +0200

    updated mysql/unit_test.sh

--- mysql/unit_test.sh
+++ mysql/unit_test.sh
@@ -13,8 +13,13 @@ echo "Sleep at least 15 seconds... MySQL needs to initialize"
 
 sleep 15
  
-echo "CREATE DATABASE db1; USE db1; CREATE TABLE x (n VARCHAR(20)); INSERT INTO TABLE x values ('muiecuceai'); SELECT * from x"
-echo "CREATE DATABASE db1; USE db1; CREATE TABLE x (n VARCHAR(20)); INSERT INTO TABLE x values ('muiecuceai'); SELECT * from x" | mysql --host=docker.angeloneacsu.com --user=root --password=pass 
+#echo "CREATE DATABASE db1; USE db1; CREATE TABLE x (n VARCHAR(20)); INSERT INTO TABLE x values ('muiecuceai'); SELECT * from x"
+echo 'CREATE DATABASE db1;' | mysql --host=docker.angeloneacsu.com --user=root --password=pass 
+
+echo 'CREATE TABLE x (n VARCHAR(20));' | mysql --host=docker.angeloneacsu.com --user=root --password=pass db1
+echo 'INSERT INTO x(n) values("muiecuceai");' | mysql --host=docker.angeloneacsu.com --user=root --password=pass db1
+
+echo 'SELECT * from x' | mysql --host=docker.angeloneacsu.com --user=root --password=pass db1 | grep -w muiecuceai 
 
 echo "Delete lamp-mysql-unittest container"
 docker rm -f lamp-mysql-unittest
