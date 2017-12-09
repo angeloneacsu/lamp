e72b09c87ca402a37f0b6e9b47e147aed0dcac66
Angelo Neacsu <angelo.neacsu@gmail.com>
2017-12-09 19:44:54 +0200

    update unit_test.sh

--- apache/unit_test.sh
+++ apache/unit_test.sh
@@ -1,1 +1,4 @@
-docker run docker.angeloneacsu.com/kilabs/lamp-apache:latest 'curl -Lv http://localhost:80 && echo $?'
+#!/bin/bash
+
+# docker run docker.angeloneacsu.com/kilabs/lamp-apache:latest 'curl -Lv http://localhost:80 && echo $?'
+curl -Lv http://localhost:80 && echo $?
