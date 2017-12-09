2564ea1cdbc860f3264d01da8f658409d13075af
Angelo Neacsu <angelo.neacsu@gmail.com>
2017-12-09 20:04:26 +0200

    updated apache/unit_test.sh

--- apache/unit_test.sh
+++ apache/unit_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 #docker run docker.angeloneacsu.com/kilabs/lamp-apache:latest echo "Unit test"
-docker run -it docker.angeloneacsu.com/kilabs/lamp-apache:latest "apt-get update; apt-get install -y curl; curl -Lv http://localhost:80 && echo $?"
-#curl -Lv http://localhost:80 && echo $?
+docker run --name lamp-apache-unittest -p 4504:80 docker.angeloneacsu.com/kilabs/lamp-apache:latest 
+
+apt-get update; apt-get install -y curl; 
+curl -Lv http://localhost:4504
+
+docker rm -f lamp-apache-unittest
