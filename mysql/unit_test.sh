80f37b0577882b1fff963b88e1632c0f7aabb94a
Angelo Neacsu <angelo.neacsu@gmail.com>
2017-12-10 00:28:59 +0200

    updated mysql/unit_test.sh

--- mysql/unit_test.sh
+++ mysql/unit_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysqladmin -h docker.angeloneacsu.com -P 53306 -u root status
+mysqladmin -h docker.angeloneacsu.com -u root --password=pass -P 53306 status
 
-mysql --host=docker.angeloneacsu.com -P 53306 --user=root --password=pass  < mysql/unit_test.sql
+mysql --host=docker.angeloneacsu.com -P 53306 -u root --password=pass  < mysql/unit_test.sql
 
