ac997ba4f5afcce0de317f70a9bc783a2d24d468
Angelo Neacsu <angelo.neacsu@gmail.com>
2017-12-10 02:48:10 +0200

    updated create-docker-compose.sh

--- create-docker-compose.sh
+++ create-docker-compose.sh
@@ -1,9 +1,11 @@
+#!/bin/sh
+
 cat > stack-deploy.yml <<EOF
 version: "3.3"
 
 services:
    backend:
-      image: ${env.DOCKER_REPO}:${env.DOCKER_REPO_PORT}/${env.CLIENT}/lamp-mysql:latest
+      image: ${DOCKER_REPO}:${DOCKER_REPO_PORT}/${CLIENT}/lamp-mysql:latest
       ports:
          - "50080:80"
       deploy:
@@ -11,7 +13,7 @@ services:
           limits:
             memory: 512M
    frontend:
-      image: ${env.DOCKER_REPO}:${env.DOCKER_REPO_PORT}/${env.CLIENT}/lamp-apache:latest
+      image: ${DOCKER_REPO}:${DOCKER_REPO_PORT}/${CLIENT}/lamp-apache:latest
       ports:
          - "53306:3306"
       deploy:
